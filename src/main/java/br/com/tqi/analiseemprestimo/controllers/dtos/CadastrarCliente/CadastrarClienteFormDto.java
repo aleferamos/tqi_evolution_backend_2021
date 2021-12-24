5bf08fcc52aa8a049e8e1f575ddab34f95f19f3a
Alefe Patrick <alefepdias@gmail.com>
2021-12-24 17:26:27 -0400

    1#card-75265805 Criar o cadsatro de clientes

---
+++ src/main/java/br/com/tqi/analiseemprestimo/controllers/dtos/CadastrarCliente/CadastrarClienteFormDto.java
@@ -0,0 +1,15 @@
+package br.com.tqi.analiseemprestimo.controllers.dtos.CadastrarCliente;
+
+import br.com.tqi.analiseemprestimo.controllers.dtos.cliente.ClienteFormDto;
+import br.com.tqi.analiseemprestimo.controllers.dtos.endereco.EnderecoFormDto;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class CadastrarClienteFormDto {
+
+    private ClienteFormDto cliente;
+    private EnderecoFormDto endereco;
+}
