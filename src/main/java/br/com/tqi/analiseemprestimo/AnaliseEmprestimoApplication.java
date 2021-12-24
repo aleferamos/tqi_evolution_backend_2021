7855ed77c0ab2e233bdfa8abe6aecb75af13f562
Alefe Patrick <alefepdias@gmail.com>
2021-12-24 14:26:06 -0400

    estrutura spring projeto

---
+++ src/main/java/br/com/tqi/analiseemprestimo/AnaliseEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tqi.analiseemprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseEmprestimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tqi/analiseemprestimo/AnaliseEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tqi.analiseemprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
