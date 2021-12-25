803c59454c97de3fcea77560925c6f8b76f953b0
Alefe Patrick <alefepdias@gmail.com>
2021-12-25 13:15:08 -0400

    Corrigindo excepetions

--- src/main/java/br/com/tqi/analiseemprestimo/models/Endereco.java
+++ src/main/java/br/com/tqi/analiseemprestimo/models/Endereco.java
@@ -27,4 +27,5 @@ public class Endereco {
 
     @Column(name = "cep")
     private String cep;
+
 }
--- src/main/java/br/com/tqi/analiseemprestimo/services/EnderecoService.java
+++ src/main/java/br/com/tqi/analiseemprestimo/services/EnderecoService.java
@@ -2,11 +2,13 @@ package br.com.tqi.analiseemprestimo.services;
 
 import br.com.tqi.analiseemprestimo.controllers.dtos.endereco.EnderecoDto;
 import br.com.tqi.analiseemprestimo.controllers.dtos.endereco.EnderecoFormDto;
+import br.com.tqi.analiseemprestimo.exceptions.RegraDeNegocioException;
 import br.com.tqi.analiseemprestimo.models.Endereco;
 import br.com.tqi.analiseemprestimo.repositories.EnderecoRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 public class EnderecoService {
@@ -21,8 +23,22 @@ public class EnderecoService {
     }
 
     public EnderecoDto save(EnderecoFormDto enderecoFormDto){
+        if(isEmpty(enderecoFormDto) == true){
+            throw new RegraDeNegocioException("cliente.campoVazio");
+        }
         Endereco endereco = modelMapper.map(enderecoFormDto, Endereco.class);
         EnderecoDto enderecoDto = modelMapper.map(enderecoRepository.save(endereco), EnderecoDto.class);
         return enderecoDto;
+    }
+
+    Boolean  isEmpty (EnderecoFormDto enderecoFormDto){
+        Boolean isEmpty = false;
+        if(enderecoFormDto.getApelido().isEmpty()
+           || enderecoFormDto.getRua().isEmpty()
+           || enderecoFormDto.getNumero().isEmpty()
+           || enderecoFormDto.getCep().isEmpty()){
+            isEmpty = true;
+        }
+        return isEmpty;
     }
 }
