a09df3d51ca59a018f126be8b402f9ae0d2c1f1f
Alefe Patrick <alefepdias@gmail.com>
2021-12-26 15:09:11 -0400

    Alterando mensagem e instalando swagger

--- src/main/java/br/com/tqi/analiseemprestimo/AnaliseEmprestimoApplication.java
+++ src/main/java/br/com/tqi/analiseemprestimo/AnaliseEmprestimoApplication.java
@@ -2,8 +2,10 @@ package br.com.tqi.analiseemprestimo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
 public class AnaliseEmprestimoApplication {
 
 	public static void main(String[] args) {
