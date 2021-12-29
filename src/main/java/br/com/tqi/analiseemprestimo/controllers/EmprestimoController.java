1b2b2aedb180b2cc8bed628b3a4e78fb1cca13f4
Alefe Patrick <alefepdias@gmail.com>
2021-12-28 21:54:20 -0400

    Removendo imports não utilizados

--- src/main/java/br/com/tqi/analiseemprestimo/controllers/EmprestimoController.java
+++ src/main/java/br/com/tqi/analiseemprestimo/controllers/EmprestimoController.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
